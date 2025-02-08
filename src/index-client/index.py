4273a5a435c124c6d9dbb89131e96a10c68d9caa
Francisco Moura <franciscoeduardomoura@gmail.com>
2025-02-08 16:50:19 -0300

    feat(service): Add proper api call handling

--- src/index-client/index.py
+++ src/index-client/index.py
@@ -1,5 +1,4 @@
 import aiohttp
-import asyncio
 
 
 class IndexNotFoundException(Exception):
@@ -8,14 +7,27 @@ class IndexNotFoundException(Exception):
         super().__init__(message)
         self.message = message
 
-async def get_index_value(number):
+class IndexProtocolException(Exception):
+    """Exception raised when protocol is not respected."""
+    def __init__(self, message=""):
+        super().__init__(message)
+        self.message = message
+
+class IndexService:
+    def __init__(self, api_url):
+        self.api_url = api_url
+
+async def get_index_value(self,number):
     async with aiohttp.ClientSession() as session:
-        async with session.get(f"https://api.example.com/index/{number}") as response:
+        async with session.get(f"{self.api_url}{number}") as response:
             if response.status == 200:
                 result = await response.json()
                 if "data" in result and "message" in result["data"] :
                     raise IndexNotFoundException(result["data"]["message"])
                 else:
-                    return result["data"]
+                    if "data" in result:
+                        return result["data"]
+                    else :
+                        raise IndexProtocolException("'data' key not found in response")
             else:
                 return "Error retrieving value"
