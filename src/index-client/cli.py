0c7b091221faec342f472e00cfad295431b66341
Francisco Moura <franciscoeduardomoura@gmail.com>
2025-02-10 06:34:27 -0300

    feat(cli): Improve arguments handling

--- src/index-client/cli.py
+++ src/index-client/cli.py
@@ -2,6 +2,14 @@ import argparse
 import asyncio
 from index import IndexService, IndexNotFoundException, IndexProtocolException
 
+
+def check_number_range(value):
+    number = int(value)
+    if number < 0 or number > 1000000:
+        raise argparse.ArgumentTypeError(f"{value} is not in the range [0-1000000]")
+    return number
+
+
 async def fetch_index_value(service, number):
     try:
         value = await service.get_index_value(number)
@@ -13,16 +21,17 @@ async def fetch_index_value(service, number):
     except Exception as e:
         print(f"An unexpected error occurred for {number}: {str(e)}")
 
+
 async def main():
     parser = argparse.ArgumentParser(description="Index client sample application using Python")
-    parser.add_argument('numbers', type=int, nargs='+', choices=range(0, 100),
-                        metavar="[0-100]", 
-                        help='A list of numbers ranging from 0 till 100')
-    parser.add_argument('url', type=str, default="http://localhost:8000",
+    parser.add_argument('numbers', type=check_number_range, nargs='+',
+                        metavar="[0-1000000]",
+                        help='A list of numbers ranging from 0 till 1000000')
+    parser.add_argument('--url', type=str, default="http://localhost:8000",
                         help='The API URL to fetch index values from')
     args = parser.parse_args()
 
-    service = IndexService(f"{args.url}/index/")
+    service = IndexService(args.url)
 
     tasks = [fetch_index_value(service, number) for number in args.numbers]
     await asyncio.gather(*tasks)
